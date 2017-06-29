04ce3237f5f5dded65d1c2977ad1423d6e60fd5e
David Fuentes <ing.davidfuentes@outlook.com>
2017-06-29 17:50:32 -0400

    Add ZingChart Library

--- public/index.php
+++ public/index.php
@@ -16,40 +16,7 @@ if (strtotime('2017-05-20') > strtotime( '2017-05-21' )) {
 }
 
 
-$string = "((:var1 + :var2) / :var3 * :var4) * 100";
 
-$response = ['ballots' => true, 'comments' => true];
-$response['ballots'] = false;
-dd(json_encode($response));
-
-$var_count = substr_count($string, ':');
-$delimiter = ' ';
-$pattern = '/[()]/';
-$replacement = '';
-$string_parts = explode($delimiter, $string) ;
-$vars = array();
-$var_name = array();
-
-for ($i = 0; $i < count($string_parts); $i++ ) {
-    // echo $string_parts[$i] . '<br/>';
-    if (stristr($string_parts[$i],   ':')) {
-       array_push($vars, preg_replace($pattern, $replacement, $string_parts[$i]));
-    }
-}
-
-
-for ($i = 0; $i < $var_count; $i++) {
-    // echo '<input type="text" name="' . $vars . '" value="">';
-    // echo '<br/>';
-}
-
-$values = ['26', '30', '60', '1'];
-
-$string = str_replace($vars, $values, $string);
-
-$compiler = new Compiler();
-$executable = $compiler->compile($string);
-$result = round($executable->run(), 2);
 
 // dd('La formula: [' . $string . ']  tiene: ' . $var_count . ' variables y da como resultado: ' . $result . '%');
 
