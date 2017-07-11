03004f5d77edf7faa6789c48848f6d68d3166f54
David Fuentes <ing.davidfuentes@outlook.com>
2017-07-11 16:41:15 -0400

    Creacion de Formulario Dinamico a partir de String

--- public/index.php
+++ public/index.php
@@ -6,19 +6,9 @@ require '../vendor/autoload.php';
 use DateTime;
 use FormulaInterpreter\Compiler;
 
-weekDays();
 
 
-if (strtotime('2017-05-20') > strtotime( '2017-05-21' )) {
-    dd('Es Mayor!');
-} else {
-    dd('Es Menor!');
-}
-
-
-
-
-// dd('La formula: [' . $string . ']  tiene: ' . $var_count . ' variables y da como resultado: ' . $result . '%');
+dd('La formula: [' . $string . ']  tiene: ' . $var_count . ' variables y da como resultado: ' . $result . '%');
 
 
 // dd(convert_min_to_days(540));
